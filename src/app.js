bdf174afe15b9cde55ab51f089fdc33769ea034f
juliaan <dhr.juliaan@hotmail.com>
2020-07-20 14:35:18 +0200

    Background pattern added

--- src/app.js
+++ src/app.js
@@ -13,14 +13,12 @@ import NameQuestion from "./components/_NameQuestion";
 let vh = window.innerHeight * 0.01; 
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
-  // We execute the same script as before
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
 let questionTypePreference = 0.5; // This number represents questiontype balance, change numner between 0 and 1, to skew the results
 
-//still needs a buttondown enter event, and maybe swipe
 const mainBtn = document.querySelector("#btn");
 mainBtn.addEventListener("click", () => {
   mainBtn.innerHTML = "Next";
@@ -85,7 +83,7 @@ const checkInput = (inputId, question) => {
   }
 };
 
-//Adds a Class to a Element
+//Adds a CSS Class to a Element
 const isCorrect = (el) => {
   el.classList.add("correct");
   console.log("Right!");
