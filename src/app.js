d874f26dac790c9bbaf626f23e6b8a9e91ce97a6
juliaan <dhr.juliaan@hotmail.com>
2020-07-25 16:54:13 +0200

    added vh fix

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import ImageQuestion from './components/_ImageQuestion';
 import NameQuestion from './components/_NameQuestion';
 
 // fix for the mobile browser viewheigth
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
   const vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
