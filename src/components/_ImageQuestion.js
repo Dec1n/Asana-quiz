5ca4b1467f5fbca3ca3de76ac26188279947e069
juliaan <dhr.juliaan@hotmail.com>
2020-07-29 12:02:05 +0200

    minor changes

--- src/components/_ImageQuestion.js
+++ src/components/_ImageQuestion.js
@@ -2,11 +2,11 @@ const ImageQuestion = (question, options) => {
   const template = `
     <div class="img-quiz-container" id="view">
       <img class="img-question" src="${question.img}">
-      <div class="name-input-container">
-      <div class="name-input" id=${options[0].id}>${options[0].sanskrit}</div>
-      <div class="name-input" id=${options[1].id}>${options[1].sanskrit}</div>
-      <div class="name-input" id=${options[2].id}>${options[2].sanskrit}</div>
-      <div class="name-input" id=${options[3].id}>${options[3].sanskrit}</div>
+        <div class="name-input-container">
+          <div class="name-input" id=${options[0].id}>${options[0].sanskrit}</div>
+          <div class="name-input" id=${options[1].id}>${options[1].sanskrit}</div>
+          <div class="name-input" id=${options[2].id}>${options[2].sanskrit}</div>
+          <div class="name-input" id=${options[3].id}>${options[3].sanskrit}</div>
       </div>
     </div>
   `;
